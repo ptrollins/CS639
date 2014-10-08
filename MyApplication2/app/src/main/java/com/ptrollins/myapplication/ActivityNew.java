b2038fbf635e9eb09c74227b134cc43fa1a784cf
Preston Rollins <preston.rollins@gmail.com>
2014-10-08 17:45:43 -0400

    added new test activity

--- MyApplication2/app/src/main/java/com/ptrollins/myapplication/ActivityNew.java
+++ MyApplication2/app/src/main/java/com/ptrollins/myapplication/ActivityNew.java
@@ -1,45 +1,36 @@
 package com.ptrollins.myapplication;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Button;
-import android.widget.TextView;
 
 
-public class MyActivity extends ActionBarActivity {
+
+public class ActivityNew extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_activity_new);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
 
-        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+    }
 
-    }
-    public void buttonOnClick(View v){
-        Button firstButton = (Button) findViewById(R.id.firstButton);
-        startActivity(new Intent(MyActivity.this, ActivityNew.class));
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.activity_new, menu);
         return true;
     }
 
@@ -66,7 +57,7 @@ public class MyActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_activity_new, container, false);
             return rootView;
         }
     }
