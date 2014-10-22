f4ab1aeefafadf3474003ec4119d09f5b0ad2b4a
Preston Rollins <preston.rollins@gmail.com>
2014-10-22 15:33:48 -0400

    Renamed Lab2 to PictureViewer

---
+++ PictureViewer/app/build/generated/source/buildConfig/debug/com/ptrollins/pictureviewer/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ptrollins.pictureviewer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.ptrollins.lab2";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
